5886f134ee28a9e666e033824b7c82a0814d1ccc
malakashehan99 <malaka19shehan44@gmail.com>
2024-07-02 02:11:51 +0530

    backend

--- ems-backend/src/main/java/com/ms/ems/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ms.ems.repository;
-
-import com.ms.ems.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
